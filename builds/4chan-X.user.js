36cbcea8b9ed1029a4e61bd3d2c7ff2c47766cb3
whatisausername <rikvidr@loves.dicksinhisan.us>
2014-02-21 16:37:44 -0500

    Update 4chan-X.user.js

--- builds/4chan-X.user.js
+++ builds/4chan-X.user.js
@@ -17,8 +17,6 @@
 // @grant        GM_openInTab
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
-// @updateURL 	 https://github.com/Spittie/4chan-x/raw/stable/builds/4chan-X.meta.js
-// @downloadURL  https://github.com/Spittie/4chan-x/raw/stable/builds/4chan-X.user.js
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAACVBMVEUAAGcAAABmzDNZt9VtAAAAAXRSTlMAQObYZgAAAF5JREFUeNrtkTESABAQxPD/R6tsE2dUGYUtFJvLDKf93KevHJAjpBorAQWSBIKqFASC4G0pCAkm4GfaEvgYXl0T6HBaE97f0vmnfYHbZOMLZCx9ISdKWwjOWZSC8GYm4SUGwfYgqI4AAAAASUVORK5CYII=
 // ==/UserScript==
 
